bc75fed7c8128b3a6b7703ab22aa7c317ea5a0ad
justindho <justinho515@gmail.com>
2021-02-20 18:08:45 -0800

    Update user goals doesn't quite work

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,6 @@ const User = require("../models/User");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("hello");
-});
-
 /**
  * Create a user
  * @param {string} req.body.username - New user's username
@@ -42,6 +38,34 @@ router.get("/getUser/:username", (req, res) => {
       goals: user.goals,
       wakeTime: user.wakeTime,
       sleepTime: user.sleepTime,
+    });
+  });
+});
+
+/**
+ * Update a user's goal
+ * @param {string} req.params.username - Requested user's username
+ * @return  {JSON} - return user's attributes
+ */
+router.put("/updateUserGoal/:username", (req, res) => {
+  const username = req.params.username;
+  const goal = req.body.goal;
+  const goalIsComplete = req.body.goalIsComplete;
+  User.findOne({ username: username }).then((user) => {
+    let smallGoals = user.goals.smallGoals;
+    let bigGoals = user.goals.bigGoals;
+    let attributeName;
+    if (smallGoals.hasOwnProperty(goal)) {
+      attributeName = `goals.smallGoals.${goal}`;
+    } else if (bigGoals.hasOwnProperty(goal)) {
+      attributeName = `goals.bigGoals.${goal}`;
+    }
+    console.log(`attributeName: ${attributeName}`);
+    User.updateOne(
+      { username: username },
+      { attributeName: goalIsComplete }
+    ).then(() => {
+      return res.json({ message: "Successfully updated user's goal" });
     });
   });
 });

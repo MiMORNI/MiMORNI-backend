75e0c5033e64941fb9aa940bf9f4bc8de45c0768
justindho <justinho515@gmail.com>
2021-02-20 22:11:28 -0800

    Add route to upload images

--- routes/media.js
+++ routes/media.js
@@ -1,32 +1,53 @@
 const express = require("express");
 const User = require("../models/User");
 const { Storage } = require("@google-cloud/storage");
+const Multer = require("multer");
+const dotenv = require("dotenv");
+const { format } = require("util");
+
+dotenv.config();
 
 const router = express.Router();
 
 const storage = new Storage();
 
-const bucketName = "mimorni-media";
+// Multer is required to process file uploads and make them available via
+// req.files.
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 50 * 1024 * 1024, // no larger than 50MB
+  },
+});
 
-router.post("/uploadImage", (req, res) => {
-  const username = req.body.username;
-  const filepath = req.body.filepath;
-  const pathParts = filepath.split("/");
-  const filename = pathParts[pathParts.length - 1];
-  uploadFile(filepath, filename);
-  makePublic(filename);
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+const bucketName = process.env.GCLOUD_STORAGE_BUCKET;
 
-  // Add image to user who uploaded it
-  const imgUrl = `https://storage.cloud.google.com/${bucketName}/${filename}`;
-  User.findOne({ username: username })
-    .then((user) => {
-      if (user.images.includes(imgUrl)) return;
-      user.images.push(imgUrl);
-      user.save();
-    })
-    .then(() => {
-      res.json({ success: true });
-    });
+router.post("/uploadImage", multer.single("image"), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send("No file uploaded.");
+    return;
+  }
+
+  // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+  });
+
+  blobStream.on("error", (err) => {
+    next(err);
+  });
+
+  blobStream.on("finish", () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    );
+    res.status(200).send(publicUrl);
+  });
+
+  blobStream.end(req.file.buffer);
 });
 
 router.get("/getUserImages/:username", (req, res) => {
